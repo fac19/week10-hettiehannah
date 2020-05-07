6d8f5b57315298f17d48526e25064f6cf97283cf
Hannah Gooding <47333850+hannahgooding@users.noreply.github.com>
2020-05-07 09:24:29 +0100

    Refactor removing repetition

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import Poster from "./poster";
 
 const Header = () => {
   const [username, setUsername] = React.useState("");
-  const [userData, setUserData] = React.useState(null);
+  const [userData, setUserData] = React.useState({});
 
   const handleUsername = (event) => setUsername(event.target.value);
 
@@ -15,58 +15,32 @@ const Header = () => {
     });
   };
 
-  if (!userData) {
-    return (
-      <div>
-        <header className="header-wrapper">
-          <h1 className="header-heading">Crimes Against The Cohort</h1>
-          <form className="header-form" onSubmit={handleSubmit}>
-            <label className="header-form__label" htmlFor="username">
-              Please enter GitHub username:
-            </label>
-            <input
-              className="header-form__input"
-              type="text"
-              id="username"
-              onChange={handleUsername}
-              value={username}
-            />
-            <button className="header-form__submit-btn" type="submit">
-              Submit
-            </button>
-          </form>
-        </header>
-      </div>
-    );
-  } else {
-    const { avatar_url, name } = userData;
-    return (
-      <div>
-        <header className="header-wrapper">
-          <h1 className="header-heading">Crimes Against The Cohort</h1>
-          <form className="header-form" onSubmit={handleSubmit}>
-            <label className="header-form__label" htmlFor="username">
-              Please enter GitHub username:
-            </label>
-            <input
-              className="header-form__input"
-              type="text"
-              id="username"
-              onChange={handleUsername}
-              value={username}
-            />
-            <button className="header-form__submit-btn" type="submit">
-              Submit
-            </button>
-          </form>
-        </header>
-
-        <main className="poster-wrapper">
-          <Poster avatar_url={avatar_url} name={name} />
-        </main>
-      </div>
-    );
-  }
+  const { avatar_url, name } = userData;
+  return (
+    <div>
+      <header className="header-wrapper">
+        <h1 className="header-heading">Crimes Against The Cohort</h1>
+        <form className="header-form" onSubmit={handleSubmit}>
+          <label className="header-form__label" htmlFor="username">
+            Please enter GitHub username:
+          </label>
+          <input
+            className="header-form__input"
+            type="text"
+            id="username"
+            onChange={handleUsername}
+            value={username}
+          />
+          <button className="header-form__submit-btn" type="submit">
+            Submit
+          </button>
+        </form>
+      </header>
+      <main className="poster-wrapper">
+        { Object.keys(userData).length ? <Poster avatar_url={avatar_url} name={name} /> : null }
+      </main>
+    </div>
+  );
 };
 
 export default Header;
