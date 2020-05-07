69c1bce75d0f146a0dc5d6dae9b5130d89321d0c
Hettie McConnell <hettie.mcconnell@outlook.com>
2020-05-07 11:49:05 +0100

    Add images and function for hiding places.
    Relates #6
    
    Co-authored-by: HannahGooding <hannah.gooding@yahoo.com>

---
+++ src/components/housing.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Housing = () => {
+  return (
+    <section className="hiding-places">
+      <h1>HELLO</h1>
+      <article className="hiding-places__article hiding-places__article1">
+        <img
+          className="hiding-places__img"
+          src="https://image.flaticon.com/icons/svg/2865/2865882.svg"
+          alt="saloon"
+        />
+      </article>
+      <article className="hiding-places__article hiding-places__article2">
+        <img
+          className="hiding-places__img"
+          src="https://image.flaticon.com/icons/svg/2893/2893003.svg"
+          alt="cactus"
+        />
+      </article>
+      <article className="hiding-places__article hiding-places__article3">
+        <img
+          className="hiding-place__img"
+          src="https://image.flaticon.com/icons/svg/2280/2280389.svg"
+          alt="carriage"
+        />
+      </article>
+    </section>
+  );
+};
+
+export default Housing;
--- src/components/poster.js
+++ src/components/poster.js
@@ -1,6 +1,7 @@
 import React from "react";
 import getCrime from "../utils/getCrime";
 import getPunishment from "../utils/getPunishment";
+import Housing from "./housing";
 
 const Poster = (props) => {
   const name = props.name;
@@ -9,18 +10,23 @@ const Poster = (props) => {
   const punishment = getPunishment();
 
   return (
-    <section className="poster">
-      <h2 className="poster-title">Wanted: {name}</h2>
-      <h3 className="poster-subtitle">Have you seen this person?</h3>
-      <img
-        className="poster-img"
-        src={avatar_url}
-        alt="profile pic of the criminal"
-      />
-      <p>Crime: {crime}</p>
-      <p>Punishment: {punishment} </p>
-      <p>Reward: Guaranteed Graduate Job</p>
-    </section>
+    <div>
+      <section className="poster">
+        <h2 className="poster-title">Wanted: {name}</h2>
+        <h3 className="poster-subtitle">Have you seen this person?</h3>
+        <img
+          className="poster-img"
+          src={avatar_url}
+          alt="profile pic of the criminal"
+        />
+        <p>Crime: {crime}</p>
+        <p>Punishment: {punishment} </p>
+        <p>Reward: Guaranteed Graduate Job</p>
+      </section>
+      <section>
+        <Housing />
+      </section>
+    </div>
   );
 };
 
