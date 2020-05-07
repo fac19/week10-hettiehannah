c186e03e32f47992a5321e449e5517b6008f7f15
Hettie McConnell <hettie.mcconnell@outlook.com>
2020-05-07 15:45:05 +0100

    Can only click on buttons once.
    Relates #6.
    
    Co-authored-by: HannahGooding <hannah.gooding@yahoo.com>

--- src/components/housing.js
+++ src/components/housing.js
@@ -1,40 +1,45 @@
 import React from "react";
 import houseRandomisation from "../utils/houseRandomisation";
 
-const resultImgSrc = [
-  "https://image.flaticon.com/icons/svg/1933/1933191.svg", // happy cowboy
-  "https://image.flaticon.com/icons/svg/1933/1933362.svg", // sad cowboy
-];
-
 const Housing = () => {
-  // const [correctButton, setCorrectButton] = React.useState("");
   const [clickedButton, setClickedButton] = React.useState("");
-
+  const [gameInPlay, setGameInPlay] = React.useState(true);
   const correctButton = houseRandomisation();
-  // console.log(correctButton);
-  console.log("clickedButton:", clickedButton);
-  function isCorrect() {
-    return clickedButton === correctButton;
-  }
+
+  React.useEffect(() => {
+    if (gameInPlay) {
+      imageDisplay();
+    }
+  }, [gameInPlay]);
 
   function imageDisplay(imageName, defaultImage) {
     if (clickedButton !== imageName) {
       return defaultImage;
     } else if (clickedButton === imageName && correctButton === imageName) {
       return (
-        <img
-          className="hiding-places__img--correct"
-          src="https://image.flaticon.com/icons/svg/1933/1933191.svg"
-          alt=""
-        />
+        <div className="hiding-places__result">
+          <img
+            className="hiding-places__img hiding-places__img--correct"
+            src="https://image.flaticon.com/icons/svg/1933/1933362.svg"
+            alt=""
+          />
+          <p className="hiding-places__output hiding-places__output--correct">
+            WELL DONE YOU CAUGHT THEM!
+          </p>
+        </div>
       );
     } else if (clickedButton === imageName && correctButton !== imageName) {
       return (
-        <img
-          className="hiding-places__img--incorrect"
-          src="https://image.flaticon.com/icons/svg/1933/1933362.svg"
-          alt=""
-        />
+        <div className="hiding-places__result">
+          <img
+            className="hiding-places__img hiding-places__img--incorrect"
+            src="https://image.flaticon.com/icons/svg/1933/1933191.svg"
+            alt=""
+          />
+          <p className="hiding-places__output hiding-places__output--incorrect">
+            They got away, bad luck!
+          </p>
+        </div>
       );
     }
   }
@@ -43,7 +48,13 @@ const Housing = () => {
     <section className="hiding-places">
       <h1>Find where they are hiding</h1>
       <article className="hiding-places__article hiding-places__article1">
-        <button onClick={() => setClickedButton("saloon")}>
+        <button
+          onClick={() => {
+            setClickedButton("saloon");
+            setGameInPlay(false);
+          }}
+          disabled={!gameInPlay}
+        >
           They are hiding behind the saloon door
           {imageDisplay(
             "saloon",
@@ -53,11 +64,16 @@ const Housing = () => {
               alt=""
             />
           )}
-          {/* Remove click-ability and refresh */}
         </button>
       </article>
       <article className="hiding-places__article hiding-places__article2">
-        <button onClick={() => setClickedButton("cactus")}>
+        <button
+          onClick={() => {
+            setClickedButton("cactus");
+            setGameInPlay(false);
+          }}
+          disabled={!gameInPlay}
+        >
           They are hiding behind the cactus
           {imageDisplay(
             "cactus",
@@ -70,7 +86,13 @@ const Housing = () => {
         </button>
       </article>
       <article className="hiding-places__article hiding-places__article3">
-        <button onClick={() => setClickedButton("carriage")}>
+        <button
+          onClick={() => {
+            setClickedButton("carriage");
+            setGameInPlay(false);
+          }}
+          disabled={!gameInPlay}
+        >
           They are hiding in the carriage
           {imageDisplay(
             "carriage",
